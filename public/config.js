612232dd5f5d28efc31df7081fdea7acf3b7716a
SbTchR <162590489+SbTchR@users.noreply.github.com>
2026-08-27 07:41:37 +0200

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.ROTATIONS_CONFIG = {
-  supabaseUrl: 'https://iinzroyxhyffdjtcyaxn.supabase.co/rest/v1/',
+  supabaseUrl: 'https://iinzroyxhyffdjtcyaxn.supabase.co',
   supabaseAnonKey: 'sb_publishable_qsDuIG11c9_zJ4M6ot7pJQ_PCrXheHN',
   allowDemo: false,
 }
